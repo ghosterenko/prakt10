39e8b4bcdcbaf43923fc5c5034c3f8a0baedc4ec
ghosterenko <arsenijkonkov243@gmail.com>
2026-04-01 19:37:39 +0500

    Create prakt10.cpp

---
+++ prakt10.cpp
@@ -0,0 +1,172 @@
+#include <iostream>
+#include <Windows.h>
+#include <string>
+
+HANDLE Th1, Th2, Th3, ThLoader;
+
+void increment() {
+    int num = 0;
+    int i = 0;
+    DWORD thId = GetCurrentThreadId();
+
+    while (true) {
+        std::cout << "Поток ID: " << thId << " - Инкремент: " << num << " - i: " << i << std::endl;
+        num++;
+        i++;
+        Sleep(1000);
+    }
+}
+
+void fibonacci() {
+    int a = 0, b = 1;
+    int i = 0;
+    DWORD thId = GetCurrentThreadId();
+
+    while (true) {
+        std::cout << "Поток ID: " << thId << " - Фибоначчи: " << a << " - i: " << i << std::endl;
+        int next = a + b;
+        a = b;
+        b = next;
+        i++;
+        Sleep(1000);
+    }
+}
+
+void factorial() {
+    int n = 0;
+    long fact = 1;
+    int i = 0;
+    DWORD thId = GetCurrentThreadId();
+
+    while (true) {
+        if (n == 0 || n == 1) {
+            fact = 1;
+        }
+        else {
+            fact = 1;
+            for (int i = 2; i <= n; i++) {
+                fact *= i;
+            }
+        }
+        std::cout << "Поток ID: " << thId << " - Факториал " << n << " = " << fact << " - i: " << i << std::endl;
+        n++;
+        i++;
+        Sleep(1000);
+    }
+}
+
+void Loader() {
+    std::cout << "Нагрузчик запущен" << std::endl;
+    SetThreadPriority(GetCurrentThread(), THREAD_PRIORITY_TIME_CRITICAL);
+    std::cout << "Максимальный приоритет" << std::endl;
+    int l = 0;
+    for (int i = 0; i < 1000000; i++)
+    {
+        l++;
+    }
+    std::cout << "Нормальный приоритет" << std::endl;
+    SetThreadPriority(GetCurrentThread(), THREAD_PRIORITY_NORMAL);
+    Sleep(3000);
+}
+
+int main() {
+    setlocale(LC_ALL, "ru");
+
+    Th1 = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)increment, NULL, 0, NULL);
+    Th2 = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)fibonacci, NULL, 0, NULL);
+    Th3 = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)factorial, NULL, 0, NULL);
+
+    SetThreadPriority(Th1, THREAD_PRIORITY_IDLE);
+    SetThreadPriority(Th2, THREAD_PRIORITY_NORMAL);
+    SetThreadPriority(Th3, THREAD_PRIORITY_HIGHEST);
+    system("cls");
+    while (true)
+    {
+        std::cout << "1. Поток 1 - низкий приоритет" << std::endl;
+        std::cout << "2. Поток 1 - нормальный приоритет" << std::endl;
+        std::cout << "3. Поток 1 - высокий приоритет" << std::endl;
+        std::cout << "4. Поток 1 - фоновый приоритет" << std::endl;
+
+        std::cout << "5. Поток 2 - низкий приоритет" << std::endl;
+        std::cout << "6. Поток 2 - нормальный приоритет" << std::endl;
+        std::cout << "7. Поток 2 - высокий приоритет" << std::endl;
+        std::cout << "8. Поток 2 - фоновый приоритет" << std::endl;
+
+        std::cout << "9. Поток 3 - низкий приоритет" << std::endl;
+        std::cout << "10. Поток 3 - нормальный приоритет" << std::endl;
+        std::cout << "11. Поток 3 - высокий приоритет" << std::endl;
+        std::cout << "12. Поток 3 - фоновый приоритет" << std::endl;
+
+        std::cout << "13. Поток нагрузчик" << std::endl;
+
+        int user;
+        std::cin >> user;
+        switch (user) {
+        case 1:
+            SetThreadPriority(Th1, THREAD_PRIORITY_LOWEST);
+            std::cout << "Потоку 1 приоритет низкий" << std::endl;
+            break;
+        case 2:
+            SetThreadPriority(Th1, THREAD_PRIORITY_NORMAL);
+            std::cout << "Потоку 1 приоритет нормальный" << std::endl;
+            break;
+        case 3:
+            SetThreadPriority(Th1, THREAD_PRIORITY_HIGHEST);
+            std::cout << "Потоку 1 приоритет высокий" << std::endl;
+            break;
+        case 4:
+            SetThreadPriority(Th1, THREAD_PRIORITY_IDLE);
+            std::cout << "Потоку 1 приоритет фоновый" << std::endl;
+            break;
+        case 5:
+            SetThreadPriority(Th2, THREAD_PRIORITY_LOWEST);
+            std::cout << "Потоку 2 приоритет низкий" << std::endl;
+            break;
+        case 6:
+            SetThreadPriority(Th2, THREAD_PRIORITY_NORMAL);
+            std::cout << "Потоку 2 приоритет нормальный" << std::endl;
+            break;
+        case 7:
+            SetThreadPriority(Th2, THREAD_PRIORITY_HIGHEST);
+            std::cout << "Потоку 2 приоритет высокий" << std::endl;
+            break;
+        case 8:
+            SetThreadPriority(Th2, THREAD_PRIORITY_IDLE);
+            std::cout << "Потоку 2 приоритет фоновый" << std::endl;
+            break;
+        case 9:
+            SetThreadPriority(Th3, THREAD_PRIORITY_LOWEST);
+            std::cout << "Потоку 3 приоритет низкий" << std::endl;
+            break;
+        case 10:
+            SetThreadPriority(Th3, THREAD_PRIORITY_NORMAL);
+            std::cout << "Потоку 3 приоритет нормальный" << std::endl;
+            break;
+        case 11:
+            SetThreadPriority(Th3, THREAD_PRIORITY_HIGHEST);
+            std::cout << "Потоку 3 приоритет высокий" << std::endl;
+            break;
+        case 12:
+            SetThreadPriority(Th3, THREAD_PRIORITY_IDLE);
+            std::cout << "Потоку 3 приоритет фоновый" << std::endl;
+            break;
+        case 13:
+            ThLoader = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)Loader, NULL, 0, NULL);
+            break;
+        default:
+            std::cout << "Неверно выбранное действие!" << std::endl;
+            break;
+        }
+
+    }
+
+    WaitForSingleObject(Th1, INFINITE);
+    WaitForSingleObject(Th2, INFINITE);
+    WaitForSingleObject(Th3, INFINITE);
+
+    CloseHandle(Th1);
+    CloseHandle(Th2);
+    CloseHandle(Th3);
+
+    return 0;
+}
